d186d32304b5a9a1ffb51d72d6a7c66c38bb637e
Cesar Fabri <cesarfabri@pop-os.localdomain>
2021-09-25 20:20:10 -0300

    insert get todos

--- src/index.js
+++ src/index.js
@@ -10,7 +10,16 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find((user) => user.username === username)
+  console.log(user)
+  
+  if (!user) {
+    return response.status(400).json({ error: 'User not found'})
+  }
+
+  request.user = user
+  return next()
 }
 
 app.post('/users', (request, response) => {
@@ -21,12 +30,14 @@ app.post('/users', (request, response) => {
       username, 
       todos: []
   })
+  console.log('users')
   return response.status(201).send()
 });
 
-app.get('/todos', (request, response) => {
-  // Complete aqui
-  return response.json({name: "cesar fabri"})
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+  const { user } = request
+  
+  return response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
