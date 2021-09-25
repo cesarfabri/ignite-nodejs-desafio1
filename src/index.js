aa0dcff7a9de049f156a93772c1e976cec71348b
Cesar Fabri <cesar.fabrii@gmail.com>
2021-09-25 14:55:55 -0300

    Insert user

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body
+  users.push({
+      id: uuidv4(), // precisa ser um uuid
+      name, 
+      username, 
+      todos: []
+  })
+  return response.status(201).send()
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
+app.get('/todos', (request, response) => {
   // Complete aqui
+  return response.json({name: "cesar fabri"})
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
